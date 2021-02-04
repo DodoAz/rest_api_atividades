c3308ae861c7390e339d5963b2c9a861e3bf1fb4
Your Name <you@example.com>
2021-02-04 12:14:18 -0300

    Repositório Iniciado e adicionado models com tabelas de pessoas e atividades e criado um utils de teste para manipular os dados do bancos de dados juntamente com um banco simples

---
+++ utils.py
@@ -0,0 +1,34 @@
+from models import Pessoas, db_session
+
+
+# Insere Dados na tabela pessoas
+def insere_pessoas():
+    pessoa = Pessoas(nome='Diogo', idade=25)
+    print(pessoa)
+    pessoa.save()
+
+
+# Consulta Dados na tabela pessoas
+def consulta_pessoas():
+    pessoas = Pessoas.query.all()
+    pessoa = Pessoas.query.filter_by(nome='Diogo').first()
+    print(pessoas, pessoa.idade)
+
+
+# Altera Dados na tabela pessoas
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Douglas').first()
+    pessoa.nome = 'Azevedo'
+    pessoa.save()
+
+
+# Exclui Dados na tabela pessoas
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Douglas').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    #altera_pessoa()
+    #exclui_pessoa()
+    consulta_pessoas()
